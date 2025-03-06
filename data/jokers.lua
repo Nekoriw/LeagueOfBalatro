5d121fde644245f5ca137093bab9e3fd3b3fe213
Nekoriw <106487505+Nekoriw@users.noreply.github.com>
2025-03-06 17:36:17 +0100

    feat: Creation of a Test Joker

---
+++ data/jokers.lua
@@ -0,0 +1,22 @@
+SMODS.Joker({
+    key = "testJoker",
+    loc_txt = {
+        name = "Test Joker",
+        text = {
+            "This is a test Joker"
+        },
+    },
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    rarity = 3,
+    pos = { x = 0, y = 0 },
+    cost = 1,
+    config = {
+        extra = {
+
+        }
+    }
+})
---
+++ main.lua
@@ -0,0 +1,8 @@
+BalatroLeague = SMODS.current_mod
+
+BalatroLeague.modded_objects = {}
+BalatroLeague.space_jokers = {
+
+}
+
+SMODS.load_file("data/jokers.lua")()
