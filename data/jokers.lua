2903aa53a817cc49b5700c476ac855921d1838e0
Nekoriw <106487505+Nekoriw@users.noreply.github.com>
2025-03-06 17:55:17 +0100

    feat: add chips mult to Test Joker

--- data/jokers.lua
+++ data/jokers.lua
@@ -3,7 +3,9 @@ SMODS.Joker({
     loc_txt = {
         name = "Test Joker",
         text = {
-            "This is a test Joker"
+            "This Test Joker gains {C:chips}+#2#{} Chips",
+            "when scoring a {C:attention}High Card{}",
+            "{C:inactive}(Currently {C:chips}+#0#{C:inactive} Chips)"
         },
     },
     unlocked = true,
@@ -11,12 +13,37 @@ SMODS.Joker({
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
-    rarity = 3,
+    rarity = 1,
     pos = { x = 0, y = 0 },
     cost = 1,
     config = {
         extra = {
-
+            chips = 0,
+            chips_gain = 10
         }
-    }
+    },
+
+    calculate = function(self, card, context)
+        -- checks if scoring card is stone card
+        if context.individual and next(context.poker_hands['High Card']) then
+            card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chips_gain
+            return {
+                extra = {
+                    message = 'Plop !',
+                    colour = G.C.RED,
+                    focus = card,
+                },
+                card = card,
+            }
+        end
+
+        --adds mult after hand is played
+        if context.joker_main and card.ability.extra.chips > 0 then
+            return {
+                chips_mod = card.ability.extra.mult,
+                message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.chips } },
+                card = card
+            }
+        end
+    end
 })
