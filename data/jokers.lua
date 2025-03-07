bfd7139043dcb7fcfb82e7be201b07c1c0d70075
Nekoriw <106487505+Nekoriw@users.noreply.github.com>
2025-03-08 00:56:13 +0100

    feat : Diana Joker

--- data/jokers.lua
+++ data/jokers.lua
@@ -1,49 +1,99 @@
 SMODS.Joker({
-    key = "khazix",
-    loc_txt = {
-        name = "Kha'zix",
-        text = {
-            "Kha'zix gains {C:chips}+#1#{} Chips",
-            "if Poker Hand is a {C:attention}High Card{}",
-            "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)"
+        key = "khazix",
+        loc_txt = {
+            name = "Kha'zix",
+            text = {
+                "Kha'zix gains {C:chips}+#1#{} Chips",
+                "if Poker Hand is a {C:attention}High Card{}",
+                "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)"
+            },
         },
-    },
-    config = {
-        extra = {
-            chips = 0,
-            chips_gain = 10
-        }
-    },
-    loc_vars = function(self, info_queue, card)
-        return {
-            vars = { card.ability.extra.chips_gain, card.ability.extra.chips },
-        }
-    end,
-    unlocked = true,
-    discovered = true,
-    blueprint_compat = true,
-    eternal_compat = true,
-    perishable_compat = true,
-    rarity = 1,
-    pos = { x = 0, y = 0 },
-    cost = 1,
-
-
-    calculate = function(self, card, context)
-        if context.before and context.scoring_name == 'High Card' and not context.blueprint then
-            card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chips_gain
-            return {
-                message = 'Upgraded!',
-                colour = G.C.BLUE
+        config = {
+            extra = {
+                chips = 0,
+                chips_gain = 10
             }
-        end
+        },
+        loc_vars = function(self, info_queue, card)
+            return {
+                vars = { card.ability.extra.chips_gain, card.ability.extra.chips },
+            }
+        end,
+        unlocked = true,
+        discovered = true,
+        blueprint_compat = true,
+        eternal_compat = true,
+        perishable_compat = true,
+        rarity = 1,
+        pos = { x = 0, y = 0 },
+        cost = 1,
 
-        if context.joker_main then
-            if card.ability.extra.chips > 0 then
+
+        calculate = function(self, card, context)
+            if context.before and context.scoring_name == 'High Card' and not context.blueprint then
+                card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chips_gain
                 return {
-                    chips = card.ability.extra.chips
+                    message = 'Upgraded!',
+                    colour = G.C.BLUE
                 }
             end
+
+            if context.joker_main then
+                if card.ability.extra.chips > 0 then
+                    return {
+                        chips = card.ability.extra.chips
+                    }
+                end
+            end
         end
-    end
-})
+    },
+
+    {
+        key = "diana",
+        loc_txt = {
+            name = "Diana",
+            text = {
+                "Diana gains {C:chips}+#1#{} Chips",
+                "if Poker Hand is a {C:attention}Three of a kind{}",
+                "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)"
+            },
+        },
+        config = {
+            extra = {
+                chips = 0,
+                chips_gain = 10
+            }
+        },
+        loc_vars = function(self, info_queue, card)
+            return {
+                vars = { card.ability.extra.chips_gain, card.ability.extra.chips },
+            }
+        end,
+        unlocked = true,
+        discovered = true,
+        blueprint_compat = true,
+        eternal_compat = true,
+        perishable_compat = true,
+        rarity = 1,
+        pos = { x = 0, y = 0 },
+        cost = 1,
+
+
+        calculate = function(self, card, context)
+            if context.before and context.scoring_name == 'Three of a kind' and not context.blueprint then
+                card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chips_gain
+                return {
+                    message = 'Upgraded!',
+                    colour = G.C.BLUE
+                }
+            end
+
+            if context.joker_main then
+                if card.ability.extra.chips > 0 then
+                    return {
+                        chips = card.ability.extra.chips
+                    }
+                end
+            end
+        end
+    })
--- main.lua
+++ main.lua
@@ -1,8 +1,3 @@
 BalatroLeague = SMODS.current_mod
 
-BalatroLeague.modded_objects = {}
-BalatroLeague.space_jokers = {
-
-}
-
 SMODS.load_file("data/jokers.lua")()
