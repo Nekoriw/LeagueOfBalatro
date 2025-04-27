75d6fe049afd1f11a44bcc600c51a87ff52ad380
Nekoriw <106487505+Nekoriw@users.noreply.github.com>
2025-04-27 18:12:40 +0200

    feat : Shelly

--- data/Boosters/objectivePack.lua
+++ data/Boosters/objectivePack.lua
@@ -1,12 +1,19 @@
+SMODS.Atlas {
+    key = "LeagueOfBalatro_Jokers",
+    path = "LeagueOfBalatro_Jokers.png",
+    px = 71,
+    py = 95
+}
+
 -- voidgrubs
 SMODS.Consumable {
     key = 'voidgrubs',      -- key
-    set = 'tarot',          -- the set of the card: corresponds to a consumable type
+    set = 'Tarot',          -- the set of the card: corresponds to a consumable type
     loc_txt = {
         name = 'Voidgrubs', -- name of card
         text = {            -- text of card
-            'Turns {C:interest}#1#{} randoms cards',
-            'into {C:interest}Void Card{}',
+            'Turns {C:attention}#1#{} randoms cards',
+            'into {C:attention}Void Card{}',
         }
     },
     config = {
@@ -29,7 +36,77 @@ SMODS.Consumable {
         end
         return false
     end,
-    use = function(self, card, area, copier)
+    use = function(self)
+        -- Check if highlighted cards exist
+        if not G.hand.highlighted or #G.hand.highlighted == 0 then
+            return false
+        end
 
+        -- Convert 3 cards into the void cards
+        for i = 1, #G.hand.highlighted do
+            local target_card = G.hand.highlighted[i]
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.2,
+                func = function()
+                    target_card:set_ability('m_LeagueOfBalatro_void', true)
+                    return true
+                end
+            }))
+        end
+        return true
+    end,
+}
+
+-- Shelly
+SMODS.Consumable {
+    key = 'shelly',      -- key
+    set = 'Tarot',       -- the set of the card: corresponds to a consumable type
+    loc_txt = {
+        name = 'Shelly', -- name of card
+        text = {         -- text of card
+            'Turns {C:attention}#1#{} card',
+            'into {C:attention}Void Card{}',
+        }
+    },
+    config = {
+        extra = {
+            cards = 1, -- configurable value
+        }
+    },
+
+    loc_vars = function(self, info_queue, center)
+        if center and center.ability and center.ability.extra then
+            return { vars = { center.ability.extra.cards } }
+        end
+        return { vars = {} }
+    end,
+    can_use = function(self, card)
+        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
+                return true
+            end
+        end
+        return false
+    end,
+    use = function(self)
+        -- Check if highlighted cards exist
+        if not G.hand.highlighted or #G.hand.highlighted == 0 then
+            return false
+        end
+
+        -- Convert 3 cards into the void cards
+        for i = 1, #G.hand.highlighted do
+            local target_card = G.hand.highlighted[i]
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.2,
+                func = function()
+                    target_card:set_ability('m_LeagueOfBalatro_void', true)
+                    return true
+                end
+            }))
+        end
+        return true
     end,
 }
