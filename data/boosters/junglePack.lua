d4cbf4205f56016d1892371d8f6d95bd2fd989c3
Nekoriw <106487505+Nekoriw@users.noreply.github.com>
2025-06-05 14:22:20 +0200

    feat : Mountain Drake

--- data/boosters/junglePack.lua
+++ data/boosters/junglePack.lua
@@ -7,11 +7,11 @@ SMODS.Atlas {
 
 --Objective Pack
 SMODS.ConsumableType {
-    key = 'ObjectiveCard',                                 -- consumable type key
+    key = 'JungleCard',                                    -- consumable type key
 
     collection_rows = { 4, 5 },                            -- amount of cards in one page
     primary_colour = G.C.PURPLE,                           -- first color
-    secondary_colour = HEX('D94496'),                      -- second color
+    secondary_colour = HEX('2E6F40'),                      -- second color
     loc_txt = {
         collection = 'Objective Cards',                    -- name displayed in collection
         name = 'Objective Card',                           -- name displayed in badge
@@ -29,7 +29,7 @@ SMODS.Booster {
         name = "Objective Pack",
         text = {
             'Choose {C:attention}#1#{} of up to',
-            '{C:attention}#2#{} {C:D94496}Objective{} cards to',
+            '{C:attention}#2#{} {C:2E6F40}Jungle{} cards to',
             'be used immediately',
         },
         group_name = "Objective Pack",
@@ -47,7 +47,7 @@ SMODS.Booster {
     create_card = function(self, card, i)
         return SMODS.create_card({
             skip_materialize = true,
-            set = "ObjectiveCard",
+            set = "JungleCard",
             area = G.pack_cards,
         })
     end,
@@ -55,8 +55,8 @@ SMODS.Booster {
 
 -- voidgrubs
 SMODS.Consumable {
-    key = 'voidgrubs',     -- key
-    set = 'ObjectiveCard', -- the set of the card: corresponds to a consumable type
+    key = 'voidgrubs',  -- key
+    set = 'JungleCard', -- the set of the card: corresponds to a consumable type
     atlas = 'LeagueOfBalatro_Consumables',
     pos = { x = 0, y = 0 },
     loc_txt = {
@@ -121,8 +121,8 @@ SMODS.Consumable {
 
 -- Shelly
 SMODS.Consumable {
-    key = 'shelly',        -- key
-    set = 'ObjectiveCard', -- the set of the card: corresponds to a consumable type
+    key = 'shelly',     -- key
+    set = 'JungleCard', -- the set of the card: corresponds to a consumable type
     atlas = 'LeagueOfBalatro_Consumables',
     pos = { x = 1, y = 0 },
     loc_txt = {
@@ -175,49 +175,106 @@ SMODS.Consumable {
         end
         return true
     end,
+}
 
-    -- Baron Nashor
-    SMODS.Consumable {
-        key = 'nashor',        -- key
-        set = 'ObjectiveCard', -- the set of the card: corresponds to a consumable type
-        atlas = 'LeagueOfBalatro_Consumables',
-        pos = { x = 2, y = 0 },
-        loc_txt = {
-            name = 'Baron Nashor', -- name of card
-            text = {               -- text of card
-                'Give {C:attention}#1#${} for each',
-                '{C:attention}Void Card{} in full deck',
-            }
-        },
-        config = {
-            extra = {
-                money = 2,
-                total = 0,
-            }
-        },
-
-        loc_vars = function(self, info_queue, center)
-            if center and center.ability and center.ability.extra then
-                return { vars = { center.ability.extra.money } }
+-- Baron Nashor
+SMODS.Consumable {
+    key = 'nashor',     -- key
+    set = 'JungleCard', -- the set of the card: corresponds to a consumable type
+    atlas = 'LeagueOfBalatro_Consumables',
+    pos = { x = 2, y = 0 },
+    loc_txt = {
+        name = 'Baron Nashor', -- name of card
+        text = {               -- text of card
+            'Give {C:attention}#1#${} for each',
+            '{C:attention}Void Card{} in full deck',
+        }
+    },
+    config = {
+        extra = {
+            money = 2,
+            total = 0,
+        }
+    },
+    loc_vars = function(self, info_queue, center)
+        if center and center.ability and center.ability.extra then
+            return { vars = { center.ability.extra.money } }
+        end
+        return { vars = {} }
+    end,
+    can_use = function(self, card)
+        if G and G.hand and card.ability and card.ability.extra then
+            return true
+        end
+        return false
+    end,
+    use = function(self, card)
+        -- calculate number of void cards in full deck
+        local void_count = 0
+        for k, v in pairs(G.playing_cards) do
+            if SMODS.has_enhancement(v, 'm_LeagueOfBalatro_void') then
+                void_count = void_count + 1
             end
-            return { vars = {} }
-        end,
-        can_use = function(self, card)
-            if G and G.hand and card.ability and card.ability.extra then
+        end
+        ease_dollars(2 * void_count)
+    end,
+}
+
+-- Mountain Drake
+SMODS.Consumable {
+    key = 'mountainDrake', -- key
+    set = 'JungleCard',    -- the set of the card: corresponds to a consumable type
+
+    loc_txt = {
+        name = 'Mountain Drake', -- name of card
+        text = {                 -- text of card
+            'Turns {C:attention}#1#{} cards',
+            'into {C:attention}Stone Card{}',
+        }
+    },
+    config = {
+        extra = {
+            cards = 2, -- configurable value
+        }
+    },
+
+    loc_vars = function(self, info_queue, center)
+        if center and center.ability and center.ability.extra then
+            return { vars = { center.ability.extra.cards } }
+        end
+        return { vars = {} }
+    end,
+
+    can_use = function(self, card)
+        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
                 return true
             end
-            return false
-        end,
-        use = function(self, card)
-            -- calculate number of void cards in full deck
-            local void_count = 0
-            for k, v in pairs(G.playing_cards) do
-                if SMODS.has_enhancement(v, 'm_LeagueOfBalatro_void') then
-                    void_count = void_count + 1
-                end
-            end
+        end
+        return false
+    end,
 
-            ease_dollars(2 * void_count)
-        end,
-    }
+    use = function(self)
+        -- Check if highlighted cards exist
+        if not G.hand.highlighted or #G.hand.highlighted == 0 then
+            return false
+        end
+
+        -- Convert selected card into stone card
+        for i = 1, #G.hand.highlighted do
+            local target_card = G.hand.highlighted[i]
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                target_card:flip(),
+                delay = 0.2,
+                func = function()
+                    play_sound('tarot1', 1.1, 0.6)
+                    target_card:flip()
+                    target_card:set_ability("m_stone", true)
+                    return true
+                end
+            }))
+        end
+        return true
+    end,
 }
--- main.lua
+++ main.lua
@@ -2,4 +2,4 @@ LeagueOfBalatro = SMODS.current_mod
 
 SMODS.load_file("data/Jokers.lua")()
 SMODS.load_file("data/enhancements/VoidCard.lua")()
-SMODS.load_file("data/boosters/objectivePack.lua")()
+SMODS.load_file("data/boosters/junglePack.lua")()
