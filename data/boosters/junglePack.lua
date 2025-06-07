a92a6dfb09ccd65ea341b7890a53f4ba3d60e5ef
Nekoriw <106487505+Nekoriw@users.noreply.github.com>
2025-06-07 02:12:53 +0200

    feat : gromp

--- data/boosters/junglePack.lua
+++ data/boosters/junglePack.lua
@@ -427,3 +427,63 @@ SMODS.Consumable {
         return true
     end,
 }
+
+-- Gromp
+SMODS.Consumable {
+    key = 'gromp',      -- key
+    set = 'JungleCard', -- the set of the card: corresponds to a consumable type
+
+    loc_txt = {
+        name = 'Gromp', -- name of card
+        text = {        -- text of card
+            'Remove {C:attention}enhancement{} and {C:attention}seal{}',
+            'to {C:attention}#1#{} card',
+        }
+    },
+    config = {
+        extra = {
+            cards = 1, -- configurable value
+        }
+    },
+
+    loc_vars = function(self, info_queue, center)
+        if center and center.ability and center.ability.extra then
+            return { vars = { center.ability.extra.cards } }
+        end
+        return { vars = {} }
+    end,
+
+    can_use = function(self, card)
+        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
+                return true
+            end
+        end
+        return false
+    end,
+
+    use = function(self)
+        -- Check if highlighted cards exist
+        if not G.hand.highlighted or #G.hand.highlighted == 0 then
+            return false
+        end
+
+        -- Convert selected card into stone card
+        for i = 1, #G.hand.highlighted do
+            local target_card = G.hand.highlighted[i]
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                target_card:flip(),
+                delay = 0.2,
+                func = function()
+                    play_sound('tarot1', 1.1, 0.6)
+                    target_card:flip()
+                    target_card:set_ability(G.P_CENTERS.c_base)
+                    target_card:set_seal(nil)
+                    return true
+                end
+            }))
+        end
+        return true
+    end,
+}
